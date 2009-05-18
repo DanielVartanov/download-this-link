7ed717bc85c99389bd8d31fe9f58c23740feccad
Daniel Vartanov <daniel.vartanov@gmail.com>
2009-05-18 17:52:13 +0000

    URI validation enabled

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,2 +1,11 @@
 class Link < ActiveRecord::Base
+  validate :url_is_well_formed
+
+  def url_is_well_formed
+    begin
+      URI.parse self.url
+    rescue URI::InvalidURIError
+      errors.add(:url, 'Ссылка какая-то странная')
+    end
+  end
 end
