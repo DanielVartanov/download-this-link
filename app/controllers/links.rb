62ed099f7411235a5f34bb5cbaca5e7644abd41a
Daniel Vartanov <daniel.vartanov@gmail.com>
2009-05-18 17:21:32 +0000

    Links creation and indexing

--- app/controllers/links.rb
+++ app/controllers/links.rb
@@ -2,15 +2,20 @@ class Links < Application
   def new
     only_provides :html
     @link = Link.new(params[:link])
-    display @item
+    display @link
   end
 
   def create
     @link = Link.new(params[:link])
     if @link.save
-      redirect url(:link, @link)
+      redirect resource(@link)
     else
       render :new
     end
+  end
+
+  def index
+    @links = Link.all
+    display @links
   end
 end
--- config/router.rb
+++ config/router.rb
@@ -30,13 +30,13 @@ Merb::Router.prepare do
   # RESTful routes
   # resources :posts
 
+  resources :links
+
   # This is the default route for /:controller/:action/:id
   # This is fine for most cases.  If you're heavily using resource-based
   # routes, you may want to comment/remove this line to prevent
   # clients from calling your create or destroy actions with a GET
-  default_routes
-
-  resources :links
+  default_routes 
   
   # Change this for your home page to be available at /
   # match('/').to(:controller => 'whatever', :action =>'index')
