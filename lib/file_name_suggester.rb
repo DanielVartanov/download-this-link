61f98f0f0b890ba1718c1d3e1d757cad298dd698
ATitov <titov.andrey@gmail.com>
2009-07-10 12:38:46 +0600

    replace " "

--- lib/file_name_suggester.rb
+++ lib/file_name_suggester.rb
@@ -2,6 +2,7 @@ class FileNameSuggester
   def self.suggest_file_name!(url)
     suggested_file_name = File.basename(URI::parse(url).path)
     suggested_file_name.strip!
+    suggested_file_name = suggested_file_name.delete " " 
     suggested_file_name.empty? ? generate_file_name : suggested_file_name
   end
 
